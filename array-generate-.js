90acda936d618099b100e74aaa8b65c9ff1fac45
Jiho Sohn <jihos@me.com>
2022-06-26 18:48:45 -0400

    archive old script

---
+++ array-generate-.js
@@ -0,0 +1,41 @@
+const cliMkdir = [
+    'mkdir',
+        '$project',
+	    '$project/scripts/',
+	    '$project/styles/',
+	    '$project/images/',
+].join(" ")
+
+const cliTouch = [
+    'touch',
+        '$project/index.html',
+	    '$project/README.md',
+	    '$project/scripts/$project.js',
+	    '$project/styles/$project.css',
+].join(" ")
+
+const cliCd = [
+    'cd',
+        '$project',
+].join(" ")
+
+const cliCmd = [
+    'git init;',
+    'code .',
+].join(" ")
+
+// convert array to string
+const cliToString = param1 => param1.toString()
+
+const cliString1 = cliToString(cliMkdir) + " && "
+const cliString2 = cliToString(cliTouch) + " && "
+const cliString3 = cliToString(cliCd) + " && "
+const cliString4 = cliToString(cliCmd)
+
+const cliStringFinal = cliString1.concat(cliString2, cliString3, cliString4)
+
+
+/* Console */
+const renderClear = () => console.clear()
+const renderConsole = paramMessage => console.log(paramMessage)
+const renderPrompt = paramMessage => prompt(paramMessage)
